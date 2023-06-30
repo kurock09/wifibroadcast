f90bbc9ab8e4e1f462a34a6cdc8ea271b719a402
consti10 <geierconstantinabc@gmail.com>
2023-06-30 14:35:27 +0200

    stats

--- src/TxRxInstance.cpp
+++ src/TxRxInstance.cpp
@@ -13,8 +13,9 @@ TxRxInstance::TxRxInstance(std::vector<std::string> wifi_cards,Options options1)
       m_wifi_cards(std::move(wifi_cards)),
       m_radiotap_header(RadiotapHeader::UserSelectableParams{})
 {
-  m_console=wifibroadcast::log::create_or_get("WBTxRx");
   assert(!m_wifi_cards.empty());
+  m_console=wifibroadcast::log::create_or_get("WBTxRx");
+  m_console->debug(" cards:{} set_direction:{}",StringHelper::string_vec_as_string(wifi_cards),m_options.set_direction);
   m_receive_pollfds.resize(m_wifi_cards.size());
   m_rx_packet_stats.resize(m_wifi_cards.size());
   for(int i=0;i<m_wifi_cards.size();i++){
