f5c75704a565b54d8a2b2a9a2fb05e6f97dee1fc
consti10 <geierconstantinabc@gmail.com>
2023-07-02 21:52:30 +0200

    update readme

--- src/legacy/WBStreamTxUDP.h
+++ src/legacy/WBStreamTxUDP.h
@@ -15,7 +15,6 @@ class WBStreamTxUDP{
  public:
   WBStreamTxUDP(std::shared_ptr<WBTxRx> txrx,WBStreamTx::Options options,int fec_k,int in_udp_port){
     wb_tx=std::make_unique<WBStreamTx>(txrx,options);
-    // catches a common newbie mistake of forgetting that this buffers in packets
    last_udp_in_packet_ts_ms=MyTimeHelper:: get_curr_time_ms();
     // we need to buffer packets due to udp
     std::vector<std::shared_ptr<std::vector<uint8_t>>> block;
@@ -45,6 +44,7 @@ class WBStreamTxUDP{
   }
   std::unique_ptr<WBStreamTx> wb_tx;
   std::unique_ptr<SocketHelper::UDPReceiver> m_udp_in;
+  // helps to catch a common newbie mistake of forgetting that this buffers in packets
   int last_udp_in_packet_ts_ms;
  private:
 };
