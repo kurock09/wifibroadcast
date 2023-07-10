a26140959451bf5daf8a6098efe1875a177dd363
consti10 <geierconstantinabc@gmail.com>
2023-07-10 20:39:55 +0200

    add simple monitor mode script

--- scripts/simple.sh
+++ scripts/simple.sh
@@ -3,6 +3,12 @@
 # Simple script to enable monitor mode and set same frequency all together
 # Intended to be used with executables/example_hello.cpp
 
+if [ $# -eq 0 ]
+  then
+    echo "Please specify the card intended for wifibroadcast"
+    exit -1
+fi
+
 # !! Need to pass card
 MY_WIFI_CARD=$1
 
