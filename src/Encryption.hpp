2c2240ae672ec4345fa7e4559a59e6dfc991bc24
kurock09 <126346824+kurock09@users.noreply.github.com>
2023-08-02 16:03:35 +0000

    modified:   src/Encryption.hpp 	modified:   src/WBReceiver.h 	modified:   src/WBTransmitter.h

--- src/Encryption.hpp
+++ src/Encryption.hpp
@@ -24,9 +24,9 @@ class Encryptor {
  public:
   // enable a default deterministic encryption key by using std::nullopt
   // else, pass path to file with encryption keys
-  explicit Encryptor(std::optional<std::string> keypair = 'drone.key', const bool DISABLE_ENCRYPTION_FOR_PERFORMANCE = false)
+  explicit Encryptor(std::optional<std::string> keypair = "drone.key", const bool DISABLE_ENCRYPTION_FOR_PERFORMANCE = false)
       : DISABLE_ENCRYPTION_FOR_PERFORMANCE(DISABLE_ENCRYPTION_FOR_PERFORMANCE) {
-        keypair = 'drone.key';
+        keypair = "drone.key";
     if (keypair == std::nullopt) {
       // use default encryption keys
       crypto_box_seed_keypair(rx_publickey.data(), tx_secretkey.data(), DEFAULT_ENCRYPTION_SEED.data());
@@ -95,9 +95,9 @@ class Decryptor {
  public:
   // enable a default deterministic encryption key by using std::nullopt
   // else, pass path to file with encryption keys
-  explicit Decryptor(std::optional<std::string> keypair = 'gs.key', const bool DISABLE_ENCRYPTION_FOR_PERFORMANCE = false)
+  explicit Decryptor(std::optional<std::string> keypair = "gs.key", const bool DISABLE_ENCRYPTION_FOR_PERFORMANCE = false)
       : DISABLE_ENCRYPTION_FOR_PERFORMANCE(DISABLE_ENCRYPTION_FOR_PERFORMANCE) {
-        keypair = 'gs.key';
+        keypair = "gs.key";
     if (keypair == std::nullopt) {
       crypto_box_seed_keypair(tx_publickey.data(), rx_secretkey.data(), DEFAULT_ENCRYPTION_SEED.data());
       wifibroadcast::log::get_default()->debug("Using default keys");
